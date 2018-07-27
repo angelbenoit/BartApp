2015221b9d993ce402d6e02582184a86771b01cc
Angel Contreras <angelcbenoit@gmail.com>
2018-07-27 16:35:32 -0700

    add styling to live schedule, make all trains currently leaving in bold red text to be more noticable

--- src/components/LiveStationData.js
+++ src/components/LiveStationData.js
@@ -2,29 +2,54 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class LiveStationData extends Component {
-    
-    getArriving(){
+
+    getArriving() {
         let arriving = [];
-        if(this.props.live.etd){
+        if (this.props.live.etd) {
             this.props.live.etd.map(station => {
-                arriving.push(<h2 key={station.destination}>{station.destination}</h2>);
                 station.estimate.map(train => {
                     arriving.push(
-                        <div key={`${station.destination}-${train.minutes}`}>
-                            <p>Leaving in {train.minutes} minutes</p>
-                        </div>
+                        `${station.destination} ${train.minutes}`
                     )
                 })
             })
         }
-        return arriving;
+        arriving.sort(function(a, b){
+            a = a.split(" ");
+            b = b.split(" ");
+            return a[a.length-1] - b[b.length-1]
+        });
+        let sortedList = [];
+        arriving.map(item => {
+            item = item.split(" ");
+            const minutes = item[item.length-1];
+            console.log(item)
+            let data = (
+                <div className="list-item">
+                    <h3>
+                        {item[0]} {item[1].length > 2 ? item[1]: ""}
+                        {
+                            minutes === "Leaving" ?
+                            <span className="leaving-now"> is leaving</span> :
+                            <span className="leaving"> leaving in {item[item.length-1]} minutes</span>
+                        }
+                    </h3>
+                </div>
+            )
+            if(minutes === "Leaving")
+                sortedList.unshift(data);
+            else
+                sortedList.push(data);
+
+        })
+        return sortedList;
     }
     render() {
         //const stationName = this.props.liveStation.name;
         //console.log(this.props.liveStation)
         console.log(this.getArriving());
         return (
-            <div>
+            <div className="displayList">
                 <h2>Trains arriving in {this.props.live.name}</h2>
                 <div>
                     {this.getArriving()}
